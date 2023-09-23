4091a1253945b72e7ea68f5b87b4cdd2473e1755
Christian Segovia <christianfs@gmail.com>
2023-09-23 18:04:56 -0400

    Add rotation and missing rows

--- src/excel_processor.py
+++ src/excel_processor.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import openpyxl
 import xlrd
 import re
 
@@ -16,12 +15,7 @@ class ExcelProcessor:
 
         for filename in os.listdir(self.input_folder):
             print(filename)
-            if filename.lower().endswith(".xlsx"):
-                file_path = os.path.join(self.input_folder, filename)
-                result_df = pd.concat(
-                    [result_df, self.process_xlsx(file_path)], ignore_index=True
-                )
-            elif filename.lower().endswith(".xls"):
+            if filename.lower().endswith(".xls"):
                 file_path = os.path.join(self.input_folder, filename)
                 result_df = pd.concat(
                     [result_df, self.process_xls(file_path)], ignore_index=True
@@ -31,73 +25,12 @@ class ExcelProcessor:
 
         result_df.to_csv(self.output_csv, index=False)
 
-    def process_xlsx(self, file_path):
-        # Abre o arquivo Excel usando openpyxl
-        wb = openpyxl.load_workbook(file_path)
-
-        for sheet_name in wb.sheetnames:
-            sheet = wb[sheet_name]
-            start_row, start_col, end_row, end_col = self.find_table_range(sheet)
-
-            if start_row is not None and start_col is not None:
-                # Encontrou a tabela, agora podemos processar
-                table_data = []
-                for row_num in range(start_row + 1, end_row + 1):
-                    if "PHASE" in str(
-                        sheet.cell(row=row_num, column=start_col).value
-                    ) or " " in str(sheet.cell(row=row_num, column=start_col).value):
-                        continue
-
-                    row_data = [
-                        sheet.cell(row=row_num, column=start_col).value,
-                        sheet.cell(row=row_num, column=start_col + 2).value,
-                    ]
-                    table_data.append(row_data)
-
-                # Transforma a tabela em DataFrame
-                table_df = pd.DataFrame(table_data, columns=["Code", "Pour 1"])
-
-                # Remove o símbolo de porcentagem e converte para float se aplicável
-                table_df["Pour 1"] = (
-                    table_df["Pour 1"]
-                    .replace("%", "", regex=True)
-                    .apply(pd.to_numeric, errors="coerce")
-                )
-
-                # Adiciona as letras e números do 'Code' às colunas 'acronym' e 'code'
-                table_df["acronym"] = table_df["Code"].str.extract("([a-zA-Z]+)")
-                table_df["code"] = table_df["Code"].str.extract("(\d+)")
-                table_df["concentration"] = table_df[table_df["Pour 1"] > 0.0]["Pour 1"]
-
-                table_df.drop("Code", axis=1, inplace=True)
-                table_df.drop("Pour 1", axis=1, inplace=True)
-                table_df.dropna(inplace=True)
-
-                table_df["acronym"] = table_df["acronym"].apply(
-                    lambda x: re.sub(
-                        "^(ME|MT|AMT|LABOMT|LABOME|LABOAMT|LABO|LAB)", "", x.upper()
-                    )
-                )
-
-                self.output_txt.write(f"{file_path}: {sheet.name}\n{table_df}\n\n")
-
-                # Concatena os resultados com o DataFrame principal
-                result_df = pd.concat([result_df, table_df], ignore_index=True)
-            # else:
-            #     print(file_path)
-
-        # Fecha o arquivo Excel
-        wb.close()
-
-        # Retorna o DataFrame atualizado
-        return result_df
-
     def process_xls(self, file_path):
         result_df = None
         wb = xlrd.open_workbook(file_path)
 
         for sheet in wb.sheets():
-            start_row, start_col, end_row, end_col = self.find_table_range_xls(sheet)
+            start_row, start_col, end_row, _ = self.find_table_range_xls(sheet)
 
             if start_row is not None and start_col is not None:
                 table_data = []
@@ -162,38 +95,15 @@ class ExcelProcessor:
 
                 self.output_txt.write(f"{file_path}: {sheet.name}\n{table_df}\n\n")
 
-                result_df = pd.concat([result_df, table_df], ignore_index=True)
-                result_df.dropna(inplace=True)
+                if not table_df.empty:
+                    table_df = self.add_missing_rows(table_df)
+                    rotated_dfs = self.rotate_dataframe(table_df)
+                    result_df = pd.concat([result_df, rotated_dfs], ignore_index=True)
+                    result_df.dropna(inplace=True)
             # else:
             #     print(f"{file_path}: {sheet.name}")
 
         return result_df
-
-    def find_table_range(self, sheet):
-        for row_num in range(1, sheet.max_row + 1):
-            for col_num in range(1, sheet.max_column - 2):
-                if (
-                    (
-                        sheet.cell(row=row_num, column=col_num).value == "Code"
-                        and sheet.cell(row=row_num, column=col_num).value
-                        != "Code action"
-                    )
-                    and (
-                        sheet.cell(row=row_num, column=col_num + 2).value == "Pour 1"
-                        or sheet.cell_value(row_num, col_num + 2) == "%global"
-                    )
-                ) or (
-                    (
-                        sheet.cell(row=row_num, column=col_num).value == ""
-                        or sheet.cell(row=row_num, column=col_num).value == "N°MP"
-                    )
-                    and sheet.cell(row=row_num, column=col_num + 2).value == "%"
-                ):
-                    start_row, start_col = row_num, col_num
-                    end_row, end_col = sheet.max_row, col_num + 2
-                    return start_row, start_col, end_row, end_col
-
-        return None, None, None, None
 
     def find_table_range_xls(self, sheet):
         for row_num in range(1, sheet.nrows):
@@ -220,3 +130,31 @@ class ExcelProcessor:
                     return start_row, start_col, end_row, end_col
 
         return None, None, None, None
+
+    def add_missing_rows(self, df, target_rows=16):
+        current_rows = len(df)
+
+        if current_rows >= target_rows:
+            return df
+
+        rows_to_add = target_rows - current_rows
+        rows_to_add_df = pd.DataFrame([[0, 0, 0]] * rows_to_add, columns=df.columns)
+
+        df = pd.concat([df, rows_to_add_df], ignore_index=True)
+
+        return df
+
+    def rotate_dataframe(self, df):
+        num_rows = len(df)
+        rotated_dataframes = []
+
+        for i in range(num_rows):
+            rotated_df = df.copy()
+
+            rotated_values = df.values.copy()
+            rotated_values = rotated_values[-i:].tolist() + rotated_values[:-i].tolist()
+            rotated_df.iloc[:, :] = rotated_values
+
+            rotated_dataframes.append(rotated_df)
+
+        return pd.concat(rotated_dataframes)
